27bfe7268be11a5ac64b4414f3835ee768176cd4
Kevin <xxkevtvxx@gmail.com>
2020-05-11 19:12:15 +0200

    added Listener, Config file and function to block commands

--- src/main/java/net/gigaclub/protector/Main.java
+++ src/main/java/net/gigaclub/protector/Main.java
@@ -1,17 +1,65 @@
 package net.gigaclub.protector;
 
+import net.gigaclub.protector.listener.CommandListener;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+
 public final class Main extends JavaPlugin {
+
+    String prefix = "[GC Protection] ";
+    public static Main plugin;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println(prefix + "Ich werde aktiviert!");
+        registerListener();
+        setPlugin(this);
+        createConfig();
+        System.out.println(prefix + "Fertig :)");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static void setPlugin(Main plugin) {
+        Main.plugin = plugin;
+    }
+
+    public static Main getPlugin() {
+        return Main.plugin;
+    }
+
+    private void registerListener() {
+        System.out.println(prefix + "Registriere Listener zum blockieren der Commands!");
+        getServer().getPluginManager().registerEvents(new CommandListener(), this);
+    }
+
+    private void createConfig() {
+        System.out.println(prefix + "Erstelle die Config zum speichern der zu blockierenden Commands!");
+        FileConfiguration config = getConfig();
+
+        if(config.getStringList("BlockedCommands").isEmpty()) {
+            ArrayList<String> commandsToBlock = new ArrayList<>();
+            commandsToBlock.add("/pl");
+            commandsToBlock.add("/plugins");
+            commandsToBlock.add("/help");
+            commandsToBlock.add("/bukkit");
+            commandsToBlock.add("/bukkit:?");
+            commandsToBlock.add("/bukkit:about");
+            commandsToBlock.add("/bukkit:pl");
+            commandsToBlock.add("/bukkit:plugins");
+            commandsToBlock.add("/bukkti:ver");
+            commandsToBlock.add("/bukkit:version");
+            commandsToBlock.add("/me");
+            commandsToBlock.add("/?");
+            config.addDefault("BlockedCommands", commandsToBlock);
+            config.options().copyDefaults(true);
+            saveConfig();
+        }
     }
 }
---
+++ src/main/java/net/gigaclub/protector/listener/CommandListener.java
@@ -0,0 +1,26 @@
+package net.gigaclub.protector.listener;
+
+import net.gigaclub.protector.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandListener implements Listener {
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+        String message = event.getMessage();
+        Player player = event.getPlayer();
+
+        for(String command : Main.getPlugin().getConfig().getStringList("BlockedCommands")) {
+            if (message.startsWith(command)) {
+                player.sendMessage(ChatColor.RED + "Du hast keine Berechtigung diesen Command auszuführen!");
+                event.setCancelled(true);
+                break;
+            }
+        }
+    }
+
+}
